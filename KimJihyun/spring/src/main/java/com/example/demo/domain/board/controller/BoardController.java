3e876df33ccdaf5e790f47c6c80cc15fffb23dc6
jihyunkim0 <60856927+jihyunkim0@users.noreply.github.com>
2023-03-07 18:09:42 +0900

    feat: 게시물 목록 보기 완료 [BOARDx]

--- KimJihyun/spring/src/main/java/com/example/demo/domain/board/controller/BoardController.java
+++ KimJihyun/spring/src/main/java/com/example/demo/domain/board/controller/BoardController.java
@@ -1,14 +1,16 @@
 package com.example.demo.domain.board.controller;
 
 import com.example.demo.domain.board.controller.request.BoardRequest;
+import com.example.demo.domain.board.entity.Board;
 import com.example.demo.domain.board.service.BoardService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/board")
-@CrossOrigin(origins = "http://localhost:8081", allowedHeaders = "*")
+@CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
 public class BoardController {
 
     final private BoardService boardService;
@@ -22,5 +24,12 @@ public class BoardController {
         log.info("boardRegister()");
 
         boardService.register(boardRequest);
+    }
+
+    @GetMapping("/list")
+    public List<Board> boardList () {
+        log.info("boardList()");
+
+        return boardService.list();
     }
 }
